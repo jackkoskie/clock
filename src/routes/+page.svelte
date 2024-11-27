34af52c9733c39b83c3ab5a86788053ef2b29274
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-11-27 09:04:25 -0500

    add clock

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+
+	let time = $state(new Date());
+	let hours = $derived(time.getHours());
+	let minutes = $derived(time.getMinutes());
+	let seconds = $derived(time.getSeconds());
+	let ampm = $derived(hours >= 12 ? 'PM' : 'AM');
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1000);
+
+		return () => clearInterval(interval);
+	});
+</script>
+
+<main class="container">
+	<div class="flex h-screen w-screen items-center justify-center text-center">
+		<h1 class="text-[15rem] font-bold text-white">
+			{hours}:{minutes < 10 ? '0' + minutes : minutes}
+			{ampm}
+		</h1>
+	</div>
+</main>
